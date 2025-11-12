a9d4ebce99424864b9ebb852ef6a0ce0483461f8
salehahmad798 <saleh315555@gmail.com>
2025-11-12 18:19:45 +0500

    delete the refresh_token from redis

--- backend/src/config/generateToken.js
+++ backend/src/config/generateToken.js
@@ -1,8 +1,7 @@
 import jwt from "jsonwebtoken";
 import { redisClient } from "../../index.js";
-import cookieParser from "cookie-parser";
 
-export const generateToken = async (id, res) => {
+export const generateToken = async (id) => {
   const accessToken = jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "1m",
   });
@@ -11,52 +10,35 @@ export const generateToken = async (id, res) => {
     expiresIn: "7d",
   });
 
+  // store refresh token in redis
   const refreshTokenKey = `refresh_token:${id}`;
-
   await redisClient.setEx(refreshTokenKey, 7 * 24 * 60 * 60, refreshToken);
-
-  res.cookie("accessToken", accessToken, {
-    httpOnly: true, // only read for backend
-
-    // secure: true,
-    sameSite: "strict",
-    maxAge: 1 * 60 * 1000,
-  });
-  res.cookie("refreshToken", refreshToken, {
-    httpOnly: true, /// always true for refreshToken
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    sameSite: "none",
-
-    // secure : true
-  });
 
   return { accessToken, refreshToken };
 };
 
 export const verifyRefreshToken = async (refreshToken) => {
   try {
-    const dedcode = jwt.verify(refreshToken, process.env.REFRESH_SECRET);
+    const decoded = jwt.verify(refreshToken, process.env.REFRESH_SECRET);
 
-    const storedToken = await redisClient.get(`refresh_token:${dedcode.id}`);
+    const storedToken = await redisClient.get(`refresh_token:${decoded.id}`);
 
     if (storedToken === refreshToken) {
-      return dedcode;
+      return decoded;
     }
     return null;
-  } catch (error) {
+  } catch {
     return null;
   }
 };
 
-export const generateaccessToken = (id, res) => {
-  const accessToken = jwt.sign({ id }, process.env.JWT_SECRET, {
+export const generateAccessTokenOnly = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "1m",
   });
+};
 
-  res.cookie("accessToken", accessToken, {
-    httpOnly: true,
-
-    sameSite: "strict",
-    maxAge: 1 * 60 * 1000,
-  });
+// delete the refresh_token from redis
+export const revokeRefreshToken = async (UserId) => {
+  await redisClient.del(`refresh_token:${UserId}`);
 };
